afc7be64f8a88ff9c2966368132f6d31c3703be2
Ram <vaishnav.rd@gmail.com>
2019-01-15 09:05:37 +0530

    fixed button action related bug

--- app/index.js
+++ app/index.js
@@ -1,5 +1,13 @@
 import document from "document";
 import { vibration } from "haptics";
+// Disable app timeout
+import { me } from "appbit";
+
+if (me.appTimeoutEnabled) {
+ console.log("Timeout is enabled");
+}
+
+me.appTimeoutEnabled = false; // Disable timeout
 
 let homeScreen = document.getElementById("homeScreen");
 let pranayamListScreen = document.getElementById("pranayamListScreen");
@@ -30,6 +38,10 @@ function showPranayamListScreen() {
 
 function showCountdownScreen() {
   hideAll();
+  me.appTimeoutEnabled = false; // Disable timeout
+// if (!me.appTimeoutEnabled) {
+ console.log("Timeout is enabled now?" + me.appTimeoutEnabled);
+// }
   countdownScreen.style.display = "inline";
   startCountdown();
 }
@@ -49,11 +61,16 @@ document.onkeypress = function(evt) {
 };
 
 listButton.onactivate = function(evt) {
-  showPranayamListScreen();
+  console.log(homeScreen.style.display);
+  if (homeScreen.style.display === "inline" || homeScreen.style.display === "inherit") {
+    showPranayamListScreen();
+  }
 };
 
 playButton.onactivate = function(evt) {
-  showCountdownScreen();
+  if (homeScreen.style.display === "inline" || homeScreen.style.display === "inherit") {
+    showCountdownScreen();
+  }
 };
 
 function getSecondsInMinutes(seconds) {
@@ -62,25 +79,50 @@ function getSecondsInMinutes(seconds) {
   return date.toISOString().substr(14, 5);
 }
 
+function setTimerText(countdown, originalMins) {
+  let mins = getSecondsInMinutes(countdown);
+  let minsLeft = countdown/60;
+  // console.log(countdown + '..' + minsLeft + '..' + originalMins);
+  if (minsLeft <= originalMins) {
+    document.getElementById("cdTimer").text = mins;
+    if (document.getElementById("cdText").text != 'time') {
+      vibration.start("ring");
+      setTimeout(() => {
+        vibration.stop();
+        // startCountdown(i+1);
+      }, 1000);
+      document.getElementById("cdText").text = 'time';
+    }
+  } else {
+    setTimeout(() => {
+      document.getElementById("cdTimer").text = '';
+    }, 500)
+    document.getElementById("cdTimer").text = '--:--';
+    document.getElementById("cdText").text = 'get ready!';
+  }
+}
+
 function startCountdown(i = 0) {
   if (timerSetInterval) {
     clearInterval(timerSetInterval);
   }
-  // var prNameList = ['Bhastrika','KapaalBhaati','Baahya','AnulomVilom','Bharaamari','Udgeet','Pranav'];
-  // var prTimeList = [1,3,1,5,1,1,1];
-  var prNameList = ['Bhastrika','KapaalBhaati','Baahya'];
-  var prTimeList = [0.5,0.5,0.5];
+  var prNameList = ['Bhastrika','KapaalBhaati','Baahya','AnulomVilom','Bharaamari','Udgeet','Pranav'];
+  var prTimeList = [1,3,1,5,1,1,1];
+  // var prNameList = ['Bhastrika','KapaalBhaati','Baahya'];
+  // var prTimeList = [0.5,0.5,0.5];
   var header = document.getElementById("cdHeader");
   var timer = document.getElementById("cdTimer");
   if (i == prNameList.length) {
     showSummary();
     return;
   }
+  
   header.text = prNameList[i];
-  var countdown = prTimeList[i] * 60;
-  timer.text = getSecondsInMinutes(countdown--);
+  var countdown = prTimeList[i] * 60 + 5;
+  var total = countdown;
+  setTimerText(countdown--, prTimeList[i]);
   timerSetInterval = setInterval(() => {
-    timer.text = getSecondsInMinutes(countdown);
+    setTimerText(countdown, prTimeList[i]);
     if (!countdown--) {
       vibration.start("ring");
       setTimeout(() => {
